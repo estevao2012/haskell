143fdf63fdaa923b2e9ec5e3c550b9f436e0898f
Estevão Andrade <estevao@uololou.com.br>
2014-02-16 18:01:41 -0300

    Finalizado o exercicio

--- Exercicios.hs
+++ Exercicios.hs
@@ -17,8 +17,7 @@ module Exercicios where
 
 	--Exercicio 4.29
 	type Desenho = [[Char]]
-
-	padrao = [ "aba" , "aba" ]
+ 
 	inserePonto :: Int -> [Char]
 	inserePonto n
 		| n == 0 = ""
@@ -53,3 +52,54 @@ module Exercicios where
 
 	imprimeDesenho :: Int -> IO()
 	imprimeDesenho = putStr . concat . map (++"\n") . finalizaDesenho . geraDesenho
+
+	--Exercicio 4.30
+	black = ["#"]
+	white = ["."]
+
+	above :: Desenho -> Desenho -> Desenho
+	above = (++)
+
+	beside :: Desenho -> Desenho -> Desenho
+	beside = zipWith (++)
+
+	blackLine:: Int -> Desenho
+	blackLine n
+		| n==1	  = black
+		| n>1	  = black `beside` whiteLine (n-1)
+
+	whiteLine:: Int -> Desenho
+	whiteLine n
+		| n==1	  = white
+		| n>1	  = white `beside` blackLine (n-1)
+
+	blackChess:: Int -> Int -> Desenho
+	blackChess n m
+		| n==1 = blackLine m
+		| n>1	 = blackLine m `above` whiteChess (n-1) m
+
+	whiteChess:: Int -> Int -> Desenho
+	whiteChess n m
+		| n==1 = whiteLine m
+		| n>1	 = whiteLine m `above` blackChess (n-1) m
+
+	chessBoard :: Int -> Desenho
+	chessBoard n = blackChess n n
+
+
+	--Exercicio 4.32
+
+	divisao :: Integer -> Double
+	divisao n
+		| odd n == False = m
+		| otherwise = (m - 1)
+		where
+			m = fromIntegral(n)/fromIntegral(2)
+
+
+	powern :: Integer -> Integer
+	powern n
+		| odd n == False = (fromIntegral(2) ^ fromIntegral(m) ) ^ fromIntegral(2)
+		| otherwise = ( (fromIntegral(2) ^ fromIntegral(m)) ^ fromIntegral(2) ) * fromIntegral(2)
+		where
+			m = ceiling(divisao n)
